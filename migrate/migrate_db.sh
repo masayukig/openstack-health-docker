9d802b78b2c709355b988a300155ee80aeee913e
Masayuki Igawa <masayuki@igawa.io>
2018-01-25 12:17:07 +0900

    Add subunit2sql container

--- migrate/migrate_db.sh
+++ migrate/migrate_db.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+# Create DB and configure privileges
 #mysql -uroot -pmy-secret-pw -e 'CREATE DATABASE subunit CHARACTER SET utf8;'
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -e 'GRANT ALL PRIVILEGES ON '$MYSQL_DATABASE'.* to '$MYSQL_USER'@"%" IDENTIFIED BY "'$MYSQL_PASSWORD'";'
